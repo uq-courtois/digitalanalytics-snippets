26e47252d0062c6ecc7c42c674054e6dc5102491
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 09:52:16 +1000

    Create listofdict.py

---
+++ listofdict.py
@@ -0,0 +1,5 @@
+x = [
+{'Name': 'Marc', 'Age': 32, 'Height': 1.84, 'Weight': 75},
+{'Name': 'Laura', 'Age': 25, 'Height': 1.65, 'Weight': 52},
+{'Name': 'Eric', 'Age': 41, 'Height': 1.72, 'Weight': 62},
+]
