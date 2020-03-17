8e2c42f505fc0cac7c89f5f4d87af49efd5d1b49
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 17:08:38 +1000

    Update ex1-12.py

--- ex1-12.py
+++ ex1-12.py
@@ -3,7 +3,7 @@
 # Print the first five urls that contain an .au domain form the list urls
 
 # TIP 1: use a variable 'count' that starts with a value 0 at the beginning of the script
-# The count variable should update itself with +1 per iteration to keep count of the number of iterations
+# The count variable should update itself with +1 per RELEVANT iteration to keep count of the number of RELEVANT iterations
 # count += 1 (=> is adding 1 to the original value of the variable)
 
 # TIP 2: if 'break' is used in a for loop, the loops terminates immediately
