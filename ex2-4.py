57ad0569c90da804433874ee8ae950e1ad53ffe8
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 20:49:28 +1000

    Create ex2-4.py

---
+++ ex2-4.py
@@ -0,0 +1,5 @@
+### EXERCISE 4:
+
+# Read the data from instagramdata.csv (http://www.cedriccourtois.be/files/instagramdata.csv)
+# Convert the followers and following variables into proper integers (K = 1,000, M = 1,000,000)
+# Make a new csv file with the new data (name it instagram_clean.csv)
