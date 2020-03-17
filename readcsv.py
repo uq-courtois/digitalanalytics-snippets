e3634bfc7dbdf419c0343f843113187c2f2819e1
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 19:10:12 +1000

    Update readcsv.py

--- readcsv.py
+++ readcsv.py
@@ -1,6 +1,6 @@
 import csv # We need to call this module
 
-file = open('csvtutorial.csv', 'r') # CSV file 'csvtutorial' in the same path as our Python script is opened
+file = open('csvtutorial.csv', 'r') # CSV file 'csvtutorial.csv' is in the same path as our Python script
 
 importdata = csv.DictReader(file, delimiter=',') # File contents written into list of dictionaries named 'importdata'
 # Note that the delimiter is a comma. Always check what delimiter your CSV file uses. I might as well be a semi-colon ';'
