397aa053b26745fd2461d212c14e1bc2f8871a15
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 16:31:48 +1000

    Create ex1-6.py

---
+++ ex1-6.py
@@ -0,0 +1,5 @@
+### EXERCISE 6
+
+# Define a variable name1 with a value Jane
+# Build a program that prints ‘Found Jane’ when the value of name matches ‘Jane’ 
+# and ‘Jane not found’ when the value does not match ‘Jane’
