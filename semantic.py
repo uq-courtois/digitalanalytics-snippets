b416e9985742b16cc435a33283465ffff6cb9634
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 13:29:13 +1000

    Update semantic.py

--- semantic.py
+++ semantic.py
@@ -1,12 +1,12 @@
 # Goal of this program is to filter all string values from a list
 # named originallist and store them in a new list named stringlist
 # At the end, the contents of the variable stringlist should be printed
-# just once. Correct the program so it does that, and only that...
+# just once. Correct the program so it does that, and only that.
 
 originallist = [4,3,'Erik',5,4,'Johanna',3,1,'Marcela']
 stringlist = []
 
 for item in originallist:
-	if type(item) == str:
-		stringlist.append(item)
-		print(stringlist)
+  if type(item) == str:
+    stringlist.append(item)
+    print(stringlist)
