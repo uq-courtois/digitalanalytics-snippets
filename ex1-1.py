ec984b4159c85eef5d89c91c45085124bb52aae9
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 14:53:16 +1000

    Create ex1-1.py

---
+++ ex1-1.py
@@ -0,0 +1,6 @@
+### EXERCISE 1:
+
+# Define a string variable named ‘name’ and set value to ‘Eric’
+# Define a integer variable named ‘age’ and set value to 25
+# Define a float variable named ‘weight_kg’ and set value to 65.50
+# Print the variables to the console
