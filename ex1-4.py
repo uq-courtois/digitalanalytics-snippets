9b16ee63f188780e2358b91d7a4f5834790270c4
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 16:13:26 +1000

    Create ex1-4.py

---
+++ ex1-4.py
@@ -0,0 +1,7 @@
+# Define a dictionary with three key-value pairs:
+# Key 'name' with value 'Jane'
+# Key 'age' with value 25
+# Key 'sex' with value 'F'
+
+# Change Jane's name into 'Erica' by overwriting the value of the key 'name'
+# Print the new value of name to the console
