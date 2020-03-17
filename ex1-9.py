bb8e6801bfa469c8b600899b580190acd2f04f59
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 16:46:52 +1000

    Create ex1-9.py

---
+++ ex1-9.py
@@ -0,0 +1,7 @@
+### EXERCISE 9:
+
+# Write a program that multiplies every number in list x by 5 
+# and store those new values in a new list that you can use after the for loop is finished.
+# Print the results in descending order...
+
+x = [4,5,2,4,5,2,4]
