a16b3d7d815b19236dc45cbb183278e2156d5260
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 14:58:08 +1000

    Create ex1-2.py

---
+++ ex1-2.py
@@ -0,0 +1,7 @@
+### EXERCISE 2:
+
+# Define a string variable named ‘surname’ and set value to ‘Jones’
+# Make a new variable ‘fullname’ by concatenating name and surname (with a whitespace in between)
+# Make a new variable named ‘weight_pound’ with Eric’s weight in pounds (1kg = 2.20462 pounds)
+# Print the variables fullname and weight_pound to the console 
+# (i.e.  The full name followed by “weighs” and then the person’s weight in pounds)
