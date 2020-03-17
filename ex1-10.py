848cb8e49a231ae8756e2a51d33684b0edfd3082
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 16:48:56 +1000

    Create ex1-10.py

---
+++ ex1-10.py
@@ -0,0 +1,6 @@
+### EXERCISE 10:
+
+# Variable lyric is a string. Loop through the individual words.
+# Only print the word ‘rock’ to the console each time it is mentioned.
+
+lyric = "Everybody rock your body Everybody rock your body right Backstreet's Back alright"
