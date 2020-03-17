8760011a3ee7680dc21a17e7ceefdff45899cc06
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 20:35:33 +1000

    Create ex2-1.py

---
+++ ex2-1.py
@@ -0,0 +1,4 @@
+### EXERCISE 1:
+
+# Read data from dataset.csv - Download it from http://www.cedriccourtois.be/files/dataset.csv
+# Print the artists and songs in the dataset to the console, line by line
