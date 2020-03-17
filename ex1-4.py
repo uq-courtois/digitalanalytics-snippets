0c5dd6c3e44ae7426889153de80e5f585581b3e6
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 15:39:23 +1000

    Create ex1-4.py

---
+++ ex1-4.py
@@ -0,0 +1,10 @@
+# Define a list with the following words: ‘cow’, ‘sheep’, ‘horse’ and print it to the console
+# Define a list of dictionaries, based on the matrix (below) and print it to the console
+
+# Name  Couse   Grade
+# Mark  Math    A
+# Mark  English A
+# Eric  Math    B
+# Eric  English B
+
+# From that dictionary, print Eric’s and Mark’s math scores to the console
