0faeb718a99b3e1180e426adcb8cfe924c95e8ce
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 16:51:59 +1000

    Create ex11-1.py

---
+++ ex11-1.py
@@ -0,0 +1,12 @@
+### EXERCISE 11:
+
+# Format the following matrix in the appropriate data structure
+
+# Name  Year of Birth
+# Mark  1976
+# Jane  1999
+# Eric  2002
+
+# Write a program that transforms the years of birth to years of age
+# Add age as a new key in the already given list of dictionaries
+# Print the name and the age of every person if they are over 30 years old
