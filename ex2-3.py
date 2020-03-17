2e0bfcef4106fef903e4f69f9913f13042c0352e
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 20:46:04 +1000

    Create ex2-3.py

---
+++ ex2-3.py
@@ -0,0 +1,4 @@
+# Read the data from searchresults.csv (http://www.cedriccourtois.be/files/searchresults.csv)
+# Print the unique domain names extracted from all results (url variable in the data file)
+# Count the number of times ‘hln.be’ is in the results
+# Make a new csv file with only the top 3 ranked results (name it searchresults_top3.csv)
