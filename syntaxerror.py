991285da29b0d299e168b656953dcfd9e8ab0980
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 12:36:10 +1000

    Create syntaxerror.py

---
+++ syntaxerror.py
@@ -0,0 +1,16 @@
+# The code below is riddled with common mistakes that lead to syntax errors
+# Run the and try to correct all of them so the code runs flawlessly
+
+print('this is some text)
+
+numbers = [1,4,3,2]
+
+for item in numbers
+  print(item)
+  
+mydata = [
+{'name'='Peter','age'=34}
+{'name'='Lisa','age'=31}
+]
+
+print(mydata[0][name]))
