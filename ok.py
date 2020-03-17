26576eadb27f53f4ae369e94e8848d352301c05c
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 18:36:59 +1000

    Create ok.py

---
+++ ok.py
@@ -0,0 +1,4 @@
+Name;Quote
+Rihanna;If someone is right for you, you'll know it.
+Lady Gaga;Vanity can create a very cruel space for you if you don't know how to manage it.
+Jay-Z;Belief in oneself and knowing who you are, I mean, that's the foundation for everything great.
