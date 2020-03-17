247c4cee0d0b617a8735b5165718df357701d837
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 20:11:28 +1000

    Update writecsv.py

--- writecsv.py
+++ writecsv.py
@@ -12,12 +12,11 @@ csv_writer.writerow(['Course','Grade']) # Write the first row, containing variab
 csv_file.close # Close file
 
 for item in newdata: # To write into the CSV, row by row...
-
-     csv_file = open('newfile.csv','a') # Open file, allow to append
-     csv_writer = csv.writer(csv_file, delimiter=';') # Not necessary, but reset delimiter just to be sure...
-     csv_writer.writerow([
+  csv_file = open('newfile.csv','a') # Open file, allow to append
+  csv_writer = csv.writer(csv_file, delimiter=';') # Not necessary, but reset delimiter just to be sure...
+  csv_writer.writerow([
                          item['Course'],
                          item['Grade'],
-                         ]) # Write result row
-     csv_file.flush() # Force to flush
-     csv_file.close # Close file
+                         ]) # Write result row, based on the two keys in each dictionary
+  csv_file.flush() # Force to flush
+  csv_file.close # Close file
