5c7d7ac7228d87533b72ec96f9dc676f8118fa64
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 17:07:22 +1000

    Create ex1-12.py

---
+++ ex1-12.py
@@ -0,0 +1,25 @@
+### EXERCISE 12a - A harder exercise...
+
+# Print the first five urls that contain an .au domain form the list urls
+
+# TIP 1: use a variable 'count' that starts with a value 0 at the beginning of the script
+# The count variable should update itself with +1 per iteration to keep count of the number of iterations
+# count += 1 (=> is adding 1 to the original value of the variable)
+
+# TIP 2: if 'break' is used in a for loop, the loops terminates immediately
+
+urls = [
+"www.abc.net.au/",
+"www.cnn.com/",
+"10play.com.au/",
+"www.freeview.com.au/",
+"www.freeview.com.au/",
+"www.bbc.co.uk/",
+"www.ariacharts.com.au/",
+"www.9now.com.au/",
+"www.yourtv.com.au/"
+]
+
+### EXERCISE 12b - Adding just one extra bit...
+
+# Print the first five unique urls that contain an .au domain form the list urls (i.e., www.freeview.com.au/ is in there twice)
