0e91f33238a24c624966ae62cf786084e4356846
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 20:41:45 +1000

    Create ex2-2.py

---
+++ ex2-2.py
@@ -0,0 +1,10 @@
+### EXERCISE 2: 
+
+# Name    Couse     Grade
+# Mark    Math      A
+# Mark    English   A
+# Eric    Math      B
+# Eric    English   B
+
+# Enter data from table into your script (i.e., hard code it)
+# Write it into a new csv file ‘grades.csv’
