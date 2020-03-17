5acf5bd2ffcf3279b9b2650554b448c0abfb09a3
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 16:39:36 +1000

    Create ex1-8.py

---
+++ ex1-8.py
@@ -0,0 +1,9 @@
+### EXERCISE 8:
+
+# Write a program that filters strings from the following list and
+# stores each of the string values in a new list called newcollection: 
+
+# TIP: test whether value is a string >>>
+# if type(item) == str:
+
+collection = ['Eric',10,34,'Karen',32,'Lisa']
