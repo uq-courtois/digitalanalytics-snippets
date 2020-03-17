4c093a37a47aae14d525f5b208c550e64df9515e
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 15:33:55 +1000

    Create ex1-3.py

---
+++ ex1-3.py
@@ -0,0 +1,9 @@
+### EXERCISE 3:
+
+# Define an empty list with the name ‘cities’
+# Add the following cities:  ‘Brisbane’, ‘Melbourne’, ‘Sydney’, ‘Perth’
+# Delete ‘Melbourne’
+# Print the list to the console
+# Print the first city to the console
+# Print the last city to the console
+# Check (programmatically!) whether Brisbane is in the list
