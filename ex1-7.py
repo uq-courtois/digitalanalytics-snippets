86331f5990aa6eba0420356b3fed35f358a52b95
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 16:36:02 +1000

    Create ex1-7.py

---
+++ ex1-7.py
@@ -0,0 +1,9 @@
+### EXERCISE 7a
+
+# Define a list with the following names: Jane, John, Marisa, Kobe, Eliza
+# Print each name on a separate line by looping through the list
+
+### EXERCISE 7b
+
+# Define a list with the following numbers: 34, 45, 23, 49
+# Divide each number by 2 and print the result on a seperate line
