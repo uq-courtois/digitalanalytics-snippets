6f1a5d9015dd0dc741e857ddd98b182393e77c54
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-17 18:29:48 +1000

    Update readcsv.py

--- readcsv.py
+++ readcsv.py
@@ -3,7 +3,7 @@ import csv # We need to call this module
 file = open('csvtutorial.csv', 'r') # CSV file 'csvtutorial' in the same path as our Python script is opened
 
 importdata = csv.DictReader(file, delimiter=',') # Filecontents written into list of dictionaries names importdata
-# Note that the delimiter is a comma. Always check what delimiter your CSV file uses. I might as well be a semi-colon ;
+# Note that the delimiter is a comma. Always check what delimiter your CSV file uses. I might as well be a semi-colon ';'
 
 for item in importdata: # We loop through the data to get a grasp of it and/or to process/manipulate it further...
     print(item['Name'],'is',item['Age'],'years old')
