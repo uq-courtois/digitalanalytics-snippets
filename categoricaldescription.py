5c139a2a83de39705724ec67e3d8e6c2938f8c82
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-03 08:38:15 +1000

    Update categoricaldescription.py

--- categoricaldescription.py
+++ categoricaldescription.py
@@ -1,16 +1,27 @@
-import pandas as pd
-import researchpy as rp
+import pandas as pd # To handle/analyse data
+import researchpy as rp # To generate concise tables
+import matplotlib.pyplot as plt # To create visualisations
 
 df = pd.read_csv('imdb_data_clean.csv', delimiter = ';') 
 
 pd.set_option('max_rows', 9999) # Set minimum of rows to show, in/decrease to needs
 
-### Mode, frequencies, counts of categorical variable
-print(rp.summary_cat(df['productionlocation'])) 
+### MODE, FREQUENCIES, AND COUNTS
+catsum = rp.summary_cat(df['productionlocation'])
+print(catsum)
+# Mode is value with highest count: 'USA'
+# Counts are absolute frequencies
+# Percentages are relative frequencies
 
-# > Mode is value with highest count: 'USA'
-# > Counts are absolute frequencies
-# > Percentages are relative frequencies
+### BART CHART
+plt.bar(catsum['Outcome'],catsum['Count'])
+# x-labels based on outcome strings of catsum
+# bar height based on count figures of catsum
+
+# Saving the image to a file
+plt.savefig('productionlocation-barchart.pdf')
+
+# For extensive customisation options of bar charts in plt: see https://matplotlib.org/3.1.0/api/_as_gen/matplotlib.pyplot.bar.html
 
 print()
 
