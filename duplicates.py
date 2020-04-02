5f42a22d86af89fba5614253b7d6475616ba084a
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-02 13:09:27 +1000

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,4 +1,4 @@
-print(df[df['title'] == 'Lady and the Tramp'][['title','year']] # lady and the Tramp before
+print(df[df['title'] == 'Lady and the Tramp'][['title','year']]) # lady and the Tramp before
 
 ### Get rid of exact duplicates 
 
@@ -8,7 +8,7 @@ df = df.drop_duplicates() # Drop exact duplicates
 ### Get rid of duplicates for a subset of variables
 ### In this case records that have the same movie title, year of production, and duration
 
-print(duplicated((subset=['title','year','duration']).sum()) # Number of duplicates with same title, year, duration
+print(df.duplicated(subset=['title','year','duration']).sum()) # Number of duplicates with same title, year, duration
 df = df.drop_duplicates(subset=['title','year','duration']) # Drop these duplicates
 
-[df['title'] == 'Lady and the Tramp'][['title','year']] # lady and the Tramp after
+print(df[df['title'] == 'Lady and the Tramp'][['title','year']]) # lady and the Tramp after
