545a5b1266029e8b62a40866c143fc2ad686c144
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-03 09:00:17 +1000

    Create numericdescription.py

---
+++ numericdescription.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('imdb_data_clean.csv', delimiter = ';') 
+
+pd.set_option('max_rows', 9999) # Set minimum of rows to show, in/decrease to needs
+
+### MEAN, STANDARD DEVIATION, MEDIAN
+print(df['duration'].describe())
+
+### HISTOGRAM
+# Render histogram (By increasing the bins number, you smoothen the graph)
+plt.hist(df['duration'], bins=100)
+
+# Save figure as pdf
+plt.savefig('histo_duration.pdf')
+
+plt.clf() # Clear figure
