93530cac0fb1e81fbe7e28187d915b46d9a572dd
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-03 12:08:40 +1000

    Update ttest.py

--- ttest.py
+++ ttest.py
@@ -9,15 +9,15 @@ df = pd.read_csv('imdb_data_clean.csv', delimiter = ';')
 
 # Subset two groups
 l_sfx = df[df['sfxgenre']=='lower sfx']['rating']
-h_hsfx = df[df['sfxgenre']=='higher sfx']['rating']
-# Plot the data
-plt.boxplot([l_sfx,h_hsfx])
-# Add labels
-plt.xticks([1, 2], ['Low SFX', 'High SFX'])
-# Save plot
-plt.savefig('boxplot.pdf')
-# Clear figure
-plt.clf()
+h_sfx = df[df['sfxgenre']=='higher sfx']['rating']
+
+means = (l_sfx.mean(),h_sfx.mean()) # Calculating means
+std = (l_sfx.std(),h_sfx.std()) # Calculating standard deviations
+positions = [0, 1] #Defining positions in the graph
+plt.bar(positions, means, yerr=std) # Compiling the plot
+plt.xticks(positions,['Low SFX', 'High SFX'],rotation="horizontal") # Adding labels
+plt.savefig("barmeanstd.pdf") # Save figure
+plt.clf() # Clear figure
 
 # Calculate and print Welch’s t-test
-print(stats.ttest_ind(l_sfx, h_hsfx, equal_var = False))
+print(stats.ttest_ind(l_sfx, h_sfx, equal_var = False))
