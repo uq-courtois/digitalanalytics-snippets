e5e0a53bdb8cf43e5be3855957523e61afb86e5a
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-03 12:25:56 +1000

    Create anova.py

---
+++ anova.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+from pingouin import welch_anova, read_dataset
+
+df = pd.read_csv('imdb_data_clean.csv', delimiter = ';') 
+
+### BAR PLOTS OF THREE GROUPS FOR A VARIABLE
+
+# Subset three groups
+usa = df[df['productionlocation']=='USA']['rating']
+coprod = df[df['productionlocation']=='Coproduction']['rating']
+nonusa = df[df['productionlocation']=='Non-USA']['rating']
+
+means = (usa.mean(),coprod.mean(),nonusa.mean()) # Calculating means
+std = (usa.std(),coprod.std(),nonusa.std()) # Calculating standard deviations
+positions = [0, 1,2] # Defining positions in the graph
+plt.bar(positions, means, yerr=std) # Compiling the plot
+plt.xticks(positions,['USA', 'Coproduction','Non-USA'],rotation="horizontal") # Adding labels
+plt.savefig("barmeanstd3+.pdf") # Save figure
+plt.clf() # Clear figure
+
+# Get descriptive table by category
+print(rp.summary_cont(df['rating'].groupby(df['productionlocation'])))
+
+# Welch's ANOVA
+
+aov = welch_anova(dv='rating', between='productionlocation', data=df)
+print(aov)
