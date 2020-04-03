096cb283ed99ef11527cf50ee745fdea55d49817
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-03 10:02:34 +1000

    Create crosstab-stackedbar.py

---
+++ crosstab-stackedbar.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('imdb_data_clean.csv', delimiter = ';') 
+
+# CROSSTABULATION
+table, results = rp.crosstab(df['sfxgenre'],df['productionlocation'], prop= 'col', test= 'chi-square')
+
+print(table) # Prints crosstab
+print()
+print(results) # Prints statistics tab
+
+# STACKED BAR CHART
+ct = pd.crosstab(df['productionlocation'],df['sfxgenre'],normalize='index') # Create PD crosstab as basis for plot
+ct.plot.bar(stacked=True) # Generate plot
+plt.legend(loc='lower left', bbox_to_anchor= (0.0, 1.01),frameon=False) # Tidy up legend (other)
+plt.xticks(rotation="horizontal") # Horizontal x-axis labels
+plt.xlabel('Production location') # Adding label on x-axis
+plt.ylabel('Proportion SFX') # Adding label on y-axis
+plt.savefig('stackedbar.pdf') # Saving figure
+plt.clf() # Clearing figure
