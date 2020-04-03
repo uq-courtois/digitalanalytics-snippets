6a0deb39762bf0204e41aac919d092c7faf81fec
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-03 10:48:24 +1000

    Create correlation.py

---
+++ correlation.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import researchpy as rp
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('imdb_data_clean.csv', delimiter = ';') 
+
+### CORRELATION
+print(rp.correlation.corr_pair(df[['year', 'rating']]))
+
+# The r-value is the magnitude of the correlation
+# p-value expresses statistical significance level
+# N reports number of cases the analysis is based on
+
+### SCATTERPLOT
+plt.scatter(df['year'],df['rating']) # Build plot
+plt.xlabel('Year of release') # x-axis label
+plt.ylabel('IMDB user rating') # y-axis label
+plt.savefig('scatterplot.pdf') # Save figure
+plt.clf() # Clear figure
