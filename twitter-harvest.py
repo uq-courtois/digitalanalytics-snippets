566991f13731075fc7a3a2bbfd88d4daed0ad727
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-20 11:00:11 +1000

    Update twitter-harvest.py

--- twitter-harvest.py
+++ twitter-harvest.py
@@ -1,14 +1,16 @@
+### DO NOT CHANGE
 from twitter_scraper import get_tweets
 from datetime import datetime, timedelta
 import pandas as pd
 import openpyxl
+###
 
 ### PARAMETERS YOU SHOULD SET:
-
 # Add @mentions or #hashtags as queries
 searchqueries = ['@ladygaga','@billieeilish','@ritaora','@beyonce']
 # Set a cut off date: how far back in time...
 cutoffdate = datetime(2020, 1, 1, 00, 00, 00)
+###
 
 ### DO NOT CHANGE ANYTHING BEYOND THIS LINE
 twitterdata = []
@@ -30,3 +32,4 @@ print('Successfully harvested',len(twitterdata),'tweets')
 
 df = pd.DataFrame(twitterdata) 
 df.to_excel('twitterdata.xlsx')
+###
