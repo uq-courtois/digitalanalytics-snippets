e9b9a4d06e49a5a562d28964cf6ea13a40e2d3b3
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-27 10:14:27 +1000

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -6,8 +6,8 @@ searchquery = "@ladygaga" # Set a searchquery, which is a string value
 # Could be a hashtage => #...
 
 for tweet in get_tweets(searchquery): # Usually runs up to 500 iterations
-	print(tweet) # Each iteration variable tweet is a dictionary with all relevant key-value pairs, for example: tweet['text'] is the key for the value of the actual tweeted text
-	print()
+  print(tweet) # Each iteration variable tweet is a dictionary with all relevant key-value pairs, for example: tweet['text'] is the key for the value of the actual tweeted text
+  print()
 
 # Adapt this script to run three consecutive queries instead of just one
 # Adapt this script so the final results are written into an xlsx file
