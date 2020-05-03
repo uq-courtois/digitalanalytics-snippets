be01f64c553e075e469e1deadfc5e42fd7ad873f
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-05-03 18:29:56 +1000

    Update cat2.py

--- cat2.py
+++ cat2.py
@@ -25,11 +25,11 @@ def location_categorisation(x):
 
   ### The conditionals that channel the actual categorisation:
 
-	if 'US' in countries and len(countries) == 1:
+	if 'USA' in countries and len(countries) == 1:
 		return 'USA'
-	if 'US' in countries and len(countries) > 1:
+	if 'USA' in countries and len(countries) > 1:
 		return 'Coproduction'
-	if 'US' not in countries and len(countries) >= 1:
+	if 'USA' not in countries and len(countries) >= 1:
 		return 'Non-USA'
     
 ######
