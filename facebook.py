34272161cf85a20e1796ecb29fdf4a45d63aeaed
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-09-24 16:56:07 +1000

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -7,18 +7,23 @@ import openpyxl
 
 ### PARAMETERS YOU SHOULD SET:
 # Add @mentions or #hashtags as queries
-pagenames = ['abc']
+pagenames = ['abc','theguardianaustralia']
 
 ### DO NOT CHANGE ANYTHING BEYOND THIS LINE
 fbdata = []
 ignorepin = 0
  
 for pagename in pagenames:
-	for post in get_posts(pagename, pages=99):
-		post['query'] = pagename
-		print(pagename,'- Harvested post',post['text'])
-		print('Posted on',post['time'])
-		fbdata.append(post)
+	for post in get_posts(pagename, pages=5):
+		print(pagename)
+		print(post['text'])
+		print(post['time'])
+
+		fbdata.append(
+			{'page':pagename,
+			'text':post['text'],
+			'time':post['time']}
+		)
 
 print('Successfully harvested',len(fbdata),'Facebook posts')
 
