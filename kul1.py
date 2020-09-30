cb004fa5c1fa1d6c69f0c45d4469e86b9108f3aa
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-09-30 15:04:21 +1000

    Create kul1.py

---
+++ kul1.py
@@ -0,0 +1,44 @@
+### Exercise 1
+
+# 1. Copy/paste these instructions into your Python file 
+# 2. Define a list named 'listx' that contains the following integer values: 5, 9, 5, 6, 7, 8
+# 3. Sort the list in a descending order (i.e., from large to small)
+# 4. Add the integer value 7 to the list
+# 5. Define a variable varx that contains the last value of the list 'listx'
+# 6. Print the multiplication of varx by 10 to the console
+# 7. Print the entire list 'listx' to the console
+
+### Exercise 2
+
+# 1. Copy/paste these instructions into your Python file 
+# 2. Define a list named 'listx' that contains the following integer values: 5, 9, 5, 6, 7, 8
+# 3. Print each number of listx divided by two 
+# 4. Store the divided numbers into a new list ‘listy’
+# 5. Print the contents of listy at the end of the program, preceded by the statement ‘The divided numbers are:’
+
+### Exercise 3
+
+# x = ['eric','lisa','lisa','marc,'john,'lisa']
+# Write a program that gets rids of x occurences of ‘lisa’ in a list and test it (program should be robust to handle an unknown number of elements ‘lisa’)
+
+### Exercise 4
+
+# 1. Define three variables: length has a value of 2, width has a value of 4, height has a value of 3
+# 2. Calculate the volume of this cuboid and print the solution (assuming the values are in m) in cubic meters: ‘The cuboid has a volume of … cubic meters’
+
+### Exercise 5
+
+quotes = [
+'The ancients dreaded death: the Christian can only fear dying. (Augustus William Hare)',
+'We who engage in nonviolent direct action are not the creators of tension. We merely bring to the surface the hidden tension that is already alive. (Martin Luther King)',
+'I have criticized absent people so often, and then discovered, to my humiliation, that I was talking with their relatives, that I have grown superstitious about that sort of thing and dropped it. (Mark Twain)',
+'The injury that we do to a man must be such that we need not fear his vengeance (Steve Perry)’
+]
+
+#1 Define a list of sentences
+#2 Print the quotes that contain we – Mind you that python is case sensitive. If you want to convert a string into lowercase, use the .lower() method – e.g.:
+
+# var = ‘This is a sentence’
+# print(var.lower())
+# >>> ‘this is a sentence’
+
