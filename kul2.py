54fae58c1003b4f580689fb21e8f04659e9bd25e
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-10-05 13:23:45 +1000

    Update kul2.py

--- kul2.py
+++ kul2.py
@@ -5,6 +5,32 @@
 # Count the number of times ‘hln.be’ is in the results
 # Make a new csv file with only the top 3 ranked results (name it searchresults_top3.csv)
 
+# Import pandas
+import pandas as pd
+
+data = pd.read_csv('searchresult.csv',sep=';') # Reading data from csv
+data = data.T.to_dict().values() # Converting dataframe into list of dictionaries
+
+urllist = [] # empty list to store all domain names we're about the extract from the page urls
+
+# Iterate through the imported data
+for item in data:
+	urllist.append(item['URL'].split('/')[0])
+# We get the value for the URL key in each iteration
+# We split on the /, which returns a list with the url in bits and pieces
+# The first bit (or element in that list), is the domain name
+# We append that domain name to urllist
+
+print(set(urllist)) # We print urllist as a set so we're sure that there are no duplicates, only uniques
+print(urllist.count('www.hln.be')) # We count the amount of times www.hln.be is in the list (this is on the list, not a set version of it)
+
+topthree = [] # empty list that will be our list of dictionaries with top three results we want to export in a file
+
+for item in data:
+	if item['Rank'] < 4: # Conditional to make sure we only add information of top three results
+		print(item['URL'],item['Rank']) # Just printing the top three resuls' URL and Rank
+		topthree.append({'URL':item['URL'],'Rank':item['Rank']}) # topthree list - empty before loop - is populated with top 3 results
+# Note that we are adding a dictionary to the list per iteration, containing the iteration value of URL and Rank
 
 ### Exercise 1
 
