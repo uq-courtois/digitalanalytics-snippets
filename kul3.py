a7952c59f089b0f251784bf5ef4ab8ec1fe8d368
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-10-11 21:18:24 +1000

    Create kul3.py

---
+++ kul3.py
@@ -0,0 +1,11 @@
+### Exercise 1
+
+# You need to scrape all the title and url combinations of the WHO news page: https://www.who.int/emergencies/diseases/novel-coronavirus-2019/media-resources/news - everything that is in the div with class name "PageContent_C003_Col01"
+# Also scrape the date
+# Print the title-url-date information in the console.
+# Write the title-url-date information in a csv file named who-news.csv. This file should contain two variables: 'title’, 'url’, and ‘date’.
+
+### Exercise 2
+
+# Scrape all the news item titles and their corresponding hyperlinks from https://nieuws.kuleuven.be/en (first five pages only)
+# Write the title-hyperlink pairs into a csv file named ‘kul-news.csv’ with two variables: ‘title' and ‘url’
