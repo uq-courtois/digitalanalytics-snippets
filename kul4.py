ade5f2943ade8511d697faa967ec758c64ff66e0
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-10-21 15:47:56 +1000

    Create kul4.py

---
+++ kul4.py
@@ -0,0 +1,13 @@
+### Exercise A
+
+# API key for Google Geocoding: ‘AIzaSyBgNKboBAT2qCTUTvC0CvJbYLhg5yIVtEY’
+# Get the geocoordinates of the Faculty of Social Sciences (KU Leuven)
+# Print them to the console (lat and lng)
+
+### Exercise B
+
+# Use the Spotify API to search for the artists listed in http://www.cedriccourtois.be/files/artists.csv.
+# From the returned JSON object: get the largest possible image url for that artist (e.g., https://i.scdn.co/image/62b33d12e2b9a033cf77585f6e3d4b2c6b3a63a1 - it is the first of the images)
+# Save each artist image as a ‘.jpg’ to your Repl.it project
+# Tip 1: Step 1 and 2 must be familiar, as they are extremely close to a prior exercise in Module 4
+# Tip 2: To save images to your project: revisit how we scraped images from a website. Once you have the url of an online image, it is easy to get/request its information and write it into a file local to your project.
