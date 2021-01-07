100b57dba9c87544072bf3e63f6e3d537d0e0d6a
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-01-07 16:25:49 +1000

    Update missing.py

--- missing.py
+++ missing.py
@@ -1,1 +1,8 @@
 print(df.isnull().sum()) # Get summary of variables' missing data count
+
+isolatemissing = pd.isnull(df['metascore'])
+# Creates a Boolean pool of missing rows to isolate the missing values for variable 'metascore'
+
+print(df[isolatemissing]) # Print rows with missing data
+
+df.dropna() # Drop rows with at least one missing entry
