f91a5f8c2e5b531e1e9d2fd0512d8d4a0da4f7cd
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-08 12:39:57 +1000

    Update writecsv_sim.py

--- writecsv_sim.py
+++ writecsv_sim.py
@@ -1,6 +1,5 @@
 # WRITING DATA TO CSV
-newdata = pd.DataFrame(variable_name)  # Converting list of dictionaries into dataframe
+newdata = pd.DataFrame(newdata)  # Converting list of dictionaries into dataframe
 newdata.to_csv('newdatafile.csv', sep=',', index=False)  # Writing dataframe into CSV file
 
-# Make sure pandas is imported at the top of your script if you don't already have it
-# The variable variable_name is where the data that you want to write is stored in your script, change this as needed
+# ! Make sure pandas is imported at the top of your script if you don't already have it
