62e23fd80eaafeeb1d55195230e9617ba9aaf378
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 16:37:46 +1000

    Update categoricaldescription.py

--- categoricaldescription.py
+++ categoricaldescription.py
@@ -19,9 +19,12 @@ plt.bar(catsum['Outcome'],catsum['Count'])
 # x-labels based on outcome strings of catsum
 # bar height based on count figures of catsum
 
+# Forces tidy plot lay-out
+plt.tight_layout()
 # Saving the image to a file
 plt.savefig('productionlocation-absolute-barchart.pdf')
-plt.clf() # Clear figure
+# Clear figure
+plt.clf() 
 
 ### BART CHART
 plt.bar(catsum['Outcome'],catsum['Percent'])
