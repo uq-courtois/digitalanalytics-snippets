87d3484756d2a528c29269ab322ea799f5c30fc5
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 16:17:58 +1000

    Update ttest.py

--- ttest.py
+++ ttest.py
@@ -3,7 +3,7 @@ import researchpy as rp
 import matplotlib.pyplot as plt
 from scipy import stats
 
-df = pd.read_csv('imdb_data_clean.csv', delimiter = ';') 
+df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/imdb_data_clean.csv', delimiter = ',') 
 
 ### BAR PLOTS OF TWO GROUPS FOR A VARIABLE
 
@@ -16,6 +16,7 @@ std = (l_sfx.std(),h_sfx.std()) # Calculating standard deviations
 positions = [0, 1] #Defining positions in the graph
 plt.bar(positions, means, yerr=std) # Compiling the plot
 plt.xticks(positions,['Low SFX', 'High SFX'],rotation="horizontal") # Adding labels
+plt.tight_layout() # Forces tidy plot lay-out
 plt.savefig("barmeanstd.pdf") # Save figure
 plt.clf() # Clear figure
 
