1b16e23464beb15b186695a4aa8dff1599930c83
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 16:12:07 +1000

    Update numericdescription.py

--- numericdescription.py
+++ numericdescription.py
@@ -2,9 +2,10 @@ import pandas as pd
 import researchpy as rp
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('imdb_data_clean.csv', delimiter = ';') 
+df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/imdb_data_clean.csv', delimiter = ',') 
 
-pd.set_option('max_rows', 9999) # Set minimum of rows to show, in/decrease to needs
+# Set max of rows to show, in/decrease to needs
+pd.set_option('max_rows', 9999) 
 
 ### MEAN, STANDARD DEVIATION (std), MEDIAN (50%)
 print(df['duration'].describe())
@@ -15,5 +16,7 @@ plt.hist(df['duration'], bins=100)
 
 # Save figure as pdf
 plt.savefig('histo_duration.pdf')
-
-plt.clf() # Clear figure
+# Forces tidy plot lay-out
+plt.tight_layout()
+# Clear figure
+plt.clf() 
