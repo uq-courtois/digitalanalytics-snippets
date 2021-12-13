39bdef7ea4247188987a3cd3f00410a0d50a481e
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 16:14:39 +1000

    Update categoricaldescription.py

--- categoricaldescription.py
+++ categoricaldescription.py
@@ -28,10 +28,10 @@ plt.bar(catsum['Outcome'],catsum['Percent'])
 # x-labels based on outcome strings of catsum
 # bar height based on percentage figures of catsum
 
-# Saving the image to a file
-plt.savefig('productionlocation-relative-barchart.pdf')
 # Forces tidy plot lay-out
 plt.tight_layout()
+# Saving the image to a file
+plt.savefig('productionlocation-relative-barchart.pdf')
 # Clear figure
 plt.clf() 
 
