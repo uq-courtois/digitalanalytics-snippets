0e7b2a4ed740e9624c58124ed982544e34dda876
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-13 16:13:56 +1000

    Update crosstab-stackedbar.py

--- crosstab-stackedbar.py
+++ crosstab-stackedbar.py
@@ -2,7 +2,10 @@ import pandas as pd
 import researchpy as rp
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('imdb_data_clean.csv', delimiter = ';') 
+df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/imdb_data_clean.csv', delimiter = ',') 
+
+# Set max of rows to show, in/decrease to needs
+pd.set_option('max_rows', 9999) 
 
 # CROSSTABULATION
 table, results = rp.crosstab(df['sfxgenre'],df['productionlocation'], prop= 'col', test= 'chi-square')
@@ -18,5 +21,8 @@ plt.legend(loc='lower left', bbox_to_anchor= (0.0, 1.01),frameon=False) # Tidy u
 plt.xticks(rotation="horizontal") # Horizontal x-axis labels
 plt.xlabel('Production location') # Adding label on x-axis
 plt.ylabel('Proportion SFX') # Adding label on y-axis
+plt.tight_layout() # Forces tidy plot lay-out
 plt.savefig('stackedbar.pdf') # Saving figure
-plt.clf() # Clearing figure
+
+# Clear figure
+plt.clf() 
