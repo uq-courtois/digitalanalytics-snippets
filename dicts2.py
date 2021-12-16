cf70e0d43898127d06dd5d550c2e6a530aee20fa
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-16 16:41:18 +1000

    Create dicts2.py

---
+++ dicts2.py
@@ -0,0 +1,14 @@
+x = [
+{'Name': 'Marc', 'Age': 32, 'Height': 1.84, 'Weight': 101},
+{'Name': 'Laura', 'Age': 25, 'Height': 1.65, 'Weight': 52},
+{'Name': 'Eric', 'Age': 41, 'Height': 1.72, 'Weight': 62},
+]
+
+# Access/print the height value in the first dict
+print(x[0)['height']
+        
+# Access/print the height value in the second dict
+print(x[1)['height']
+         
+# Access/print the height value in the third dict
+print(x[2)['height']            
