aaacd676f5f502987a13861ee462407a0afaa1fa
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-16 16:47:55 +1000

    Update dicts1.py

--- dicts1.py
+++ dicts1.py
@@ -3,6 +3,7 @@ x = [
 {'Name': 'Laura', 'Age': 25, 'Height': 1.65, 'Weight': 52},
 {'Name': 'Eric', 'Age': 41, 'Height': 1.72, 'Weight': 62},
 ]
+
 # Access/print the first dict
 print(x[0)
         
