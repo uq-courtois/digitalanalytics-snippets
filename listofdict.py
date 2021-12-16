8e66ed64e740e9b990a86657e9d564c97d96c72f
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-16 16:30:22 +1000

    Update listofdict.py

--- listofdict.py
+++ listofdict.py
@@ -1,5 +1,5 @@
 x = [
-{'Name': 'Marc', 'Age': 32, 'Height': 1.84, 'Weight': 75},
+{'Name': 'Marc', 'Age': 32, 'Height': 1.84, 'Weight': 101},
 {'Name': 'Laura', 'Age': 25, 'Height': 1.65, 'Weight': 52},
 {'Name': 'Eric', 'Age': 41, 'Height': 1.72, 'Weight': 62},
 ]
