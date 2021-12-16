067d02b15e4cee4673d4b053edbbf43bbca6c24b
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-16 16:41:36 +1000

    Update dicts1.py

--- dicts1.py
+++ dicts1.py
@@ -1,3 +1,8 @@
+x = [
+{'Name': 'Marc', 'Age': 32, 'Height': 1.84, 'Weight': 101},
+{'Name': 'Laura', 'Age': 25, 'Height': 1.65, 'Weight': 52},
+{'Name': 'Eric', 'Age': 41, 'Height': 1.72, 'Weight': 62},
+]
 # Access/print the first dict
 print(x[0)
         
@@ -5,4 +10,4 @@ print(x[0)
 print(x[1)
          
 # Access/print the third dict
-print(x[2)              
+print(x[2)
