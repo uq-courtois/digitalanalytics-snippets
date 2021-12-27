faec3712ee54ad4c3ab5acfc7dea2318943044a7
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-27 20:26:45 +1000

    Update entirecleaning.py

--- entirecleaning.py
+++ entirecleaning.py
@@ -1,67 +1,70 @@
 import pandas as pd
-
+import numpy as np
+ 
 # Step 1: Read the data
-df = pd.read_csv('http://www.digitalanalytics.id.au/static/files/imdb_data.csv', delimiter = ',')
-
+df = pd.read_csv('consolidated_omdb.csv', delimiter = ',')
+ 
 # Step 2: Get an overview of the variables
 print(df.info())
+ 
+# Step 3: Get an overview of the values
+print(df['Error'].unique())
+print(len(df['Error'].unique()))
+subset = df[df['Error'] == 'Incorrect IMDb ID.']
+print(subset['imdburl'])
 
-# Step 3: Check for duplicates
+# Step 4: Check for duplicates
 print('\n# Exact duplicates:',df.duplicated().sum()) 
 print('\n# Rows with duplicates:\n',df[df.duplicated()])
-print('\n# Shape before removing duplicates:\n',df.shape)
-df = df.drop_duplicates() # Get rid of duplicates
-print('\n# Shape after removing duplicates:\n',df.shape)
 
-# Step 4: Check for missing values
+# Step 5: Check for missing values
 print('\n # Missing data:\n',df.isnull().sum())
-isolatemissing = pd.isnull(df['year'])
-print('\n Rows with missing data in year:\n',df[isolatemissing])
+
 print('\n# Shape before removing missings:\n',df.shape)
-df = df.dropna(subset=['year']) # Get rid of observations in year with missing values
+df = df.dropna(subset=['Title','imdbRating','Country','Language']) # Get rid of observations in year with missing values
 print('\n# Shape after removing missings:\n',df.shape)
 
-# Step 5: Convert strings into numerics
-print(df['year'].head(25)) # print 25 first observations
-df['year'] = df['year'].str[-5:-1].astype(int)
-print(df['year'].head(25)) # print 25 first observations
+# Step 6: Convert strings into numerics
+print(df['Year'].head(10)) # print 10 first observations
+df['Year'] = df['Year'].str[:4].astype(int)
+print(df['Year'].head(10)) # print 10 first observations
 
 # Step 6: Create categorisations
 
-### Divide between genres prone to special effects and those less so
+### Divide between productions that contain English or do not
 
-print(df['genres'].head(25))
+print(df['Language'].head(25))
 
-def specialeffects(genres):
-	if 'Action' in genres or 'Fantasy' in genres or 'Sci-Fi' in genres or 'Adventure' in genres or 'Animation' in genres:
-		sfxgenre = 'higher sfx'
+def language_categorisation(Language):
+	if 'English' in Language:
+		english = 'yes'
 	else:
-		sfxgenre = 'lower sfx'
-	return sfxgenre
+		english = 'no'
+	return english
 	
-df['sfxgenre'] = df['genres'].apply(lambda x: specialeffects(x))
-print(df[['genres','sfxgenre']])
+df['english'] = df['Language'].apply(lambda x: language_categorisation(x))
+print(df[['Language','english']])
 
 ### Divide location of production studio in only USA, collaborations between USA and others, and only others
 
-print(df['countries'].head(25))
+print(df['Country'].head(25))
 
-def location_catergorisation(countries):
+def location_catergorisation(Country):
 	productionlocation = ''
 
-	if type(countries) == str:
-		countries = countries.split(', ')
-		if 'USA' in countries and len(countries) == 1:
+	if type(Country) == str:
+		Country = Country.split(', ')
+		if 'United States' in Country and len(Country) == 1:
 			productionlocation = 'USA only'
-		if 'USA' not in countries and len(countries) >= 1:
+		if 'United States' not in Country and len(Country) >= 1:
 			productionlocation = 'Non-USA'
-		if 'USA' in countries and len(countries) > 1:
+		if 'United States' in Country and len(Country) > 1:
 			productionlocation = 'Coproduction'
 
 		return productionlocation
 
-df['productionlocation'] = df['countries'].apply(lambda x: location_catergorisation(x))
-print(df[['productionlocation','countries']])
+df['productionlocation'] = df['Country'].apply(lambda x: location_catergorisation(x))
+print(df[['Country','productionlocation']])
 
 # Step 7: Get final overview
 print(df.info())
